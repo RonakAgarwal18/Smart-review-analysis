b728bfd119fcf28117fcd59c4034ee4fffee8125
RonakAgarwal18 <ronak2agarwal2@gmail.com>
2026-06-13 04:51:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from predict import analyze_review
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
